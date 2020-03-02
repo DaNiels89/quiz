678a60cf5c4ca347bef623d903f927bb2884575e
DaNiels89 <nielssloesen@hotmail.com>
2020-03-02 14:51:03 -0500

    Changed app.js location back

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import data from '../trivia.json';
+import Question from './Question';
+import Submit from './Submit';
+import Score from './Score';
+import Answer from './Answer';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Question />
+      <Answer />
+      <Submit />
+      <Score />
+    </div>
+  );
 }
 
 export default App;
