5f9ed2285eae3ca98d46b21729ef8bc6fb21b9e4
DaNiels89 <nielssloesen@hotmail.com>
2020-03-03 09:51:44 -0500

    Configured React Hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import data from '../trivia.json';
-import Question from './Question';
-import Submit from './Submit';
-import Score from './Score';
-import Answer from './Answer';
+// import data from './trivia.json';
+import Question from './components/Question';
+import Submit from './components/Submit';
+import Score from './components/Score';
+import Answer from './components/Answer';
 
 function App() {
   return (
