c352fbf555e174b1ed1ddb76eef941e122641d0a
DaNiels89 <nielssloesen@hotmail.com>
2020-03-03 14:23:00 -0500

    Added changes in the question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,7 +5,13 @@ import data from '../trivia.json';
 
 export default function Question() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  function onSubmit(res, e) {
+    if (res.answer === '') {
+      alert('Please select an answer');
+    } else {
+      // Todo tell that the answer is correct
+    }
+  }
 
   return (
     <div>
@@ -13,20 +19,44 @@ export default function Question() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <div>
-            <input id="answer1" name="answer" type="radio" />
-            <label>Stockholm</label>
+            <input
+              id="answer1"
+              name="answer"
+              value="Stockholm"
+              type="radio"
+              ref={register}
+            />
+            <label for="answer1">Stockholm</label>
           </div>
           <div>
-            <input id="answer2" name="answer" type="radio" />
-            <label>Brussels</label>
+            <input
+              id="answer2"
+              name="answer"
+              value="Brussels"
+              type="radio"
+              ref={register}
+            />
+            <label for="answer2">Brussels</label>
           </div>
           <div>
-            <input id="answer3" name="answer" type="radio" />
-            <label>Amsterdam</label>
+            <input
+              id="answer3"
+              name="answer"
+              value="Amsterdam"
+              type="radio"
+              ref={register}
+            />
+            <label for="answer3">Amsterdam</label>
           </div>
           <div>
-            <input id="answer4" name="answer" type="radio" />
-            <label>Oslo</label>
+            <input
+              id="answer4"
+              name="answer"
+              value="Oslo"
+              type="radio"
+              ref={register}
+            />
+            <label for="answer4">Oslo</label>
           </div>
           <input type="submit" value="Submit" />
         </fieldset>
